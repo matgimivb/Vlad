aecd57a398b59987613faf06142fabebc62549ad
Casper <>
2021-03-10 10:58:52 +0100

    Init commit

---
+++ Vlad/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module rs.edu.mg.ivb {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens rs.edu.mg.ivb to javafx.fxml;
+    exports rs.edu.mg.ivb;
+}
---
+++ Vlad/src/main/java/rs/edu/mg/ivb/PrimaryController.java
@@ -0,0 +1,12 @@
+package rs.edu.mg.ivb;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
