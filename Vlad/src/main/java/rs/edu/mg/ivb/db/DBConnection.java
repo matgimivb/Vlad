677fa8fa68e834fe5889ca93a19664cb56d332a1
Casper <>
2021-03-18 09:41:29 +0100

    Promenjena baza na MySQL

--- Vlad/src/main/java/rs/edu/mg/ivb/db/DBConnection.java
+++ Vlad/src/main/java/rs/edu/mg/ivb/db/DBConnection.java
@@ -3,10 +3,14 @@ package rs.edu.mg.ivb.db;
 import java.sql.*;
 
 public class DBConnection {
+    // SQLite
+    //public final static String DB_URL = "src/main/resources/rs/edu/mg/ivb/db/vladDB.db";
+    //public final static String CONNECTION_STRING = "jdbc:sqlite:"+DB_URL;
     
-    public final static String DB_URL = "src/main/resources/rs/edu/mg/ivb/db/vladDB.db";
-    public final static String CONNECTION_STRING = "jdbc:sqlite:"+DB_URL;
-    
+    //MySQL
+    public final static String DB_URL = "";
+    public final static String CONNECTION_STRING = "jdbc:mysql:"+DB_URL;
+
     public static Connection getConnection() throws SQLException{
         return DriverManager.getConnection(CONNECTION_STRING);
     }
